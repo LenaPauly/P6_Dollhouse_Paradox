b3bc7a64e92126e9f07dd6dff3df69589028c56b
Lena Pauly <lena.pauly@stud.h-da.de>
2023-07-08 13:21:03 +0200

    Editing AI stuff
    changed some AI scripts

--- P6_DollhouseParadox/Assets/Lena/AI_scripts/TransferMessage.cs
+++ P6_DollhouseParadox/Assets/Lena/AI_scripts/TransferMessage.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Voice;
 
 public class TransferMessage : MonoBehaviour
 {
     //Attach this script to the same object that has Photonview attached to it!!!
-    public RecorderManager RecordingManager; 
+    public RecorderManager RecordingManager;
+   
 
     private void Start()
     {
         
     }
 
-  //  void SendString(string message)
-  // {
-  //      photonView.RPC("ReceiveString", RpcTarget.Others, message);  //remote reciever call... everyone call the function and rpc.target all other players should do this. message is string(hand over with function)
-  //  }
-  //  [PunRPC]
-  //  private void ReceiveString(string receivedString)
-  //  {
-  //      RecordingManager.saveString(receivedString);
-  //      Debug.Log("Received string: " + receivedString);
-  //  }
+    void SendString(string message)
+   {
+        PhotonView.RPC("ReceiveString", RpcTarget.Others, message);  //remote reciever call... everyone call the function and rpc.target all other players should do this. message is string(hand over with function)
+    }
+    [PunRPC]
+    private void ReceiveString(string receivedString)
+    {
+        RecordingManager.saveString(receivedString);
+        Debug.Log("Received string: " + receivedString);
+    }
+
+    
 }
